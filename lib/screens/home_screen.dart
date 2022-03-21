077e251c71b82e9bdee6861024ba82ce00d4cf83
gilangpd <gilangpd634@gmail.com>
2022-03-21 14:31:24 +0700

    bagian_5

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_cookbook_navigation/screens/selection_screen.dart';
+
+
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Returning Data Demo'),
+      ),
+      // Create the SelectionButton widget in the next step.
+      body: const Center(
+        child: SelectionScreen(),
+      ),
+    );
+  }
+}
