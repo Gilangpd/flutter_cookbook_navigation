44a0e0aca9bb0a109f374d516231d227920c02b4
gilangpd <gilangpd634@gmail.com>
2022-03-21 13:24:52 +0700

    bagian_1

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'detail_screen.dart';
-
 
 class MainScreen extends StatelessWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -21,6 +19,28 @@ class MainScreen extends StatelessWidget {
         child: Hero(
           tag: 'imageHero',
           child: Image.network('https://picsum.photos/250?image=9'),
+        ),
+      ),
+    );
+  }
+}
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GestureDetector(
+        onTap: () {
+          Navigator.pop(context);
+        },
+        child: Center(
+          child: Hero(
+            tag: 'imageHero',
+            child: Image.network(
+              'https://picsum.photos/250?image=9',
+            ),
+          ),
         ),
       ),
     );
