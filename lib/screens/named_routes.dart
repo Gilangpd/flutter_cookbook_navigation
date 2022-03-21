7fd6641538c62b19ccbb70507cd4848fdb4a6195
gilangpd <gilangpd634@gmail.com>
2022-03-21 13:46:24 +0700

    bagian_3

--- lib/screens/named_routes.dart
+++ lib/screens/named_routes.dart
@@ -1,22 +1,5 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      title: 'Named Routes Demo',
-      // Start the app with the "/" named route. In this case, the app starts
-      // on the FirstScreen widget.
-      initialRoute: '/',
-      routes: {
-        // When navigating to the "/" route, build the FirstScreen widget.
-        '/': (context) => const FirstScreen(),
-        // When navigating to the "/second" route, build the SecondScreen widget.
-        '/second': (context) => const SecondScreen(),
-      },
-    ),
-  );
-}
-
 class FirstScreen extends StatelessWidget {
   const FirstScreen({Key? key}) : super(key: key);
 
@@ -28,9 +11,7 @@ class FirstScreen extends StatelessWidget {
       ),
       body: Center(
         child: ElevatedButton(
-          // Within the `FirstScreen` widget
           onPressed: () {
-            // Navigate to the second screen using a named route.
             Navigator.pushNamed(context, '/second');
           },
           child: const Text('Launch screen'),
@@ -51,10 +32,7 @@ class SecondScreen extends StatelessWidget {
       ),
       body: Center(
         child: ElevatedButton(
-          // Within the SecondScreen widget
           onPressed: () {
-            // Navigate back to the first screen by popping the current route
-            // off the stack.
             Navigator.pop(context);
           },
           child: const Text('Go back!'),
